8a23f3d4be74b5c2516c947df6ba692f1ffdc040
ItsICJ <74201832+ItsICJ@users.noreply.github.com>
2021-05-17 20:36:02 +0200

    Actualizaciones

--- proyectoweb/js/script.js
+++ proyectoweb/js/script.js
@@ -129,4 +129,26 @@ $(function(){
 });
 });
 /* END OF SCROLL TOP */
+function cargarUser() {
+	if (sessionStorage.getItem("nomUser") !== null) {
+	$("#vacio").css("display","inherit");
+	document.getElementById('vacio').innerHTML = "Perfil de " + sessionStorage.getItem("nomUser");
+	$("#vacio").css("border","1px solid");
+	$("#vacio").css("border-color","white");
+
+	$("#login").css("display","none");
+	$("#logout").css("display","initial");
+
+}
+}
+
+function logout() {
+	sessionStorage.removeItem("nomUser");
+	window.alert("Cerrado exitosamente.");
+	$("#vacio").css("display","none");
+
+	$("#login").css("display","initial");
+	$("#logout").css("display","none");
+
+}
 /* END OF CUSTOM JS */
