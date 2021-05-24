812a679f9a68325f4d14886b76114cf5c7ffe068
ItsICJ <74201832+ItsICJ@users.noreply.github.com>
2021-05-24 16:38:13 +0200

    Login

--- proyectoweb/js/login.js
+++ proyectoweb/js/login.js
@@ -10,7 +10,7 @@ function leerXML() {
       miFuncion(this);
     }
   };
-  xhr.open("GET", "https://github.com/ItsICJ/Publico/proyectoweb/xml/LM1.xml", true);
+  xhr.open("GET", "https://ItsICJ.github.io/Publico/proyectoweb/xml/LM1.xml", true);
   xhr.send();	
 }
 
