e93ac8621a4e89b9a51900961e4025cb6affe65c
ItsICJ <74201832+ItsICJ@users.noreply.github.com>
2021-05-24 16:21:53 +0200

    Actualizo dolly y login

--- proyectoweb/js/login.js
+++ proyectoweb/js/login.js
@@ -10,7 +10,7 @@ function leerXML() {
       miFuncion(this);
     }
   };
-  xhr.open("GET", "../xml/LM1.xml", true);
+  xhr.open("GET", "https://github.com/ItsICJ/Publico/tree/main/proyectoweb/xml/LM1.xml", true);
   xhr.send();	
 }
 
